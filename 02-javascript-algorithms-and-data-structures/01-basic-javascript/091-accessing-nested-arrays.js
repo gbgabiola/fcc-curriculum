982945fa263224b5e794403d8fd97024ccf8b0d6
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2019-08-08 23:57:00 +0800

    Update basic js

---
+++ 02-javascript-algorithms-and-data-structures/01-basic-javascript/091-accessing-nested-arrays.js
@@ -0,0 +1,21 @@
+// Setup
+var myPlants = [
+  { 
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }  
+];
+
+var secondTree = myPlants[1].list[1];
