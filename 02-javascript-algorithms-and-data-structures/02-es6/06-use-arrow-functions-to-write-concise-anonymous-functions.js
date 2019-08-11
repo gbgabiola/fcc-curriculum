4696db019f72a537d187e88c18617e96dcace7c4
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2019-08-12 00:02:07 +0800

    Update es6 challenges
    - Write and use arrow functions
    - Write higher order functions
    - Set default parameters for functions
    - Use the rest operator function parameters

---
+++ 02-javascript-algorithms-and-data-structures/02-es6/06-use-arrow-functions-to-write-concise-anonymous-functions.js
@@ -0,0 +1,7 @@
+// const magic = () => {
+//   "use strict";
+//   return new Date();
+// };
+
+// One line solution
+const magic = () => new Date();
---
+++ 02-javascript-algorithms-and-data-structures/02-es6/07-write-arrow-functions-with-parameters.js
@@ -0,0 +1,10 @@
+// const myConcat = (arr1, arr2) => {
+//   "use strict";
+//   return arr1.concat(arr2);
+// };
+
+// One line solution
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
