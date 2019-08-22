8a8a683092cfb1a6fca23d62500d4b6b91b2772c
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2019-08-23 00:33:03 +0800

    Complete debugging
    - Catch Mixed Usage of Single and Double Quotes
    - Catch Use of Assignment Operator Instead of Equality Operator
    - Catch Missing Open and Closing Parenthesis After a Function Call
    - Catch Arguments Passed in the Wrong Order When Calling a Function
    - Catch Off By One Errors When Using Indexing
    - Use Caution When Reinitializing Variables Inside a Loop
    - Prevent Infinite Loops with a Valid Terminal Condition

---
+++ 02-javascript-algorithms-and-data-structures/04-debugging/10-catch-off-by-one-errors-when-using-indexing.js
@@ -0,0 +1,13 @@
+/*
+  Challenge: Fix the two indexing errors in the following function so all the numbers 1 through 5 are printed to the console.
+*/
+
+function countToFive() {
+  let firstFive = "12345";
+  let len = firstFive.length;
+  for (let i = 0; i < len; i++) {
+    console.log(firstFive[i]);
+  }
+}
+
+countToFive();
