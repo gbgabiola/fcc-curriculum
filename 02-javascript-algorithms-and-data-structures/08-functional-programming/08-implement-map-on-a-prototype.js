0725cdc866e52a0a061a00f353fdcd28725512c8
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2019-09-13 01:57:55 +0800

    Update js fp section
    - Use the map Method to Extract Data from an Array
    - Implement map on a Prototype
    - Use the filter Method to Extract Data from an Array

---
+++ 02-javascript-algorithms-and-data-structures/08-functional-programming/08-implement-map-on-a-prototype.js
@@ -0,0 +1,24 @@
+/*
+  Challenge: Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map().
+  You may use a forloop or the forEachmethod.
+*/
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  var newArray = [];
+  // for (let i = 0; i < this.length; i++) newArray.push(callback(this[i])); // Using for loop
+
+  this.forEach(item => newArray.push(callback(item))); // Using forEach
+  return newArray;
+};
+
+var new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
+// 46
+// 46,130
+// 46,130,196
+// 46,130,196,10
