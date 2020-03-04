80f09edba10d9c92d823112cfee38e3a01eaeb37
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-03-04 23:10:07 +0800

    Update algorithm section
    including the previous files
    - Reverse a String
    - Factorialize a Number
    - Find the Longest Word in a String
    - Return Largest Numbers in Arrays
    - Confirm the Ending

--- 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/05-return-largest-numbers-in-arrays.js
+++ 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/05-return-largest-numbers-in-arrays.js
@@ -1,50 +1,50 @@
-/*
-  Challenge: Return an array consisting of the largest number from each provided sub-array.
-  For simplicity, the provided array will contain exactly 4 sub-arrays.
-  Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
-*/
+/**
+ * Challenge: Return an array consisting of the largest number from each provided sub-array.
+ * For simplicity, the provided array will contain exactly 4 sub-arrays.
+ *
+ * Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+ */
 
+ /**
+// Solution 1
 function largestOfFour(arr) {
   const results = [];
-  for (let i = 0; i < arr.length; i++) {
-    let largest = arr[i][0];
-    for (let j = 1; j < arr[i].length; j++) {
-      if (arr[i][j] > largest) {
-        largest = arr[i][j];
-      }
-    }
-    results[i] = largest;
-  }
 
-  // Solution 2: Using forEach, Math.max.appy & arrow function
-  // arr.forEach((a) => {
-  //   results.push(Math.max.apply(null, a));
-  // });
+  arr.forEach((a) => {
+    results.push(Math.max.apply(null, a));
+  });
   return results;
+}
+*/
 
-  // Intermediate Solution: Using map, reduce & ternary
-  return arr.map(function(group) {
-    return group.reduce(function(prev, current) {
+/**
+// Solution 2
+function largestOfFour(arr) {
+  // Step 1: Map over the arrays
+  return arr.map(subArray => { // Step 3: Return the largest numbers of each sub-arrays
+
+    // Step 2. Return the largest numbers for each sub-arrays with Math.max() method, use null instead of 'this' keyword
+    return Math.max.apply(null, subArray);
+  });
+}
+*/
+
+
+// Solution 3
+function largestOfFour(arr) {
+  return arr.map(function (group) {
+    return group.reduce(function (prev, current) {
       return (current > prev) ? current : prev;
     });
   });
-
-
-  // Advanced Solution : Using map, Function.appy, Function.bind & Math.max
-  // return arr.map(Function.apply.bind(Math.max, null));
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+/**
+// Solution 4
+function largestOfFour(arr) {
+  return arr.map(Function.apply.bind(Math.max, null));
+}
+*/
 
-// Other solutions
-// function largestOfFour(arr) {
-//   const arg = []; // to put the numbers in
-//   const len = arr.length; // how many miniarrays there are
-//   for(let i = 0; i < len; i++) { // loops the number of miniarrays
-//     let lar = 0; // to store the biggest int of four
-//     lar = arr[0]; // lar = firstnum of miniarray
-//     lar = Math.max(...arr[i]); // to find the highest value of the four
-//     arg.push(lar); // pushes biggest num on arg
-//   }
-//   return arg; // returns arg
-// }
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]); // [5, 27, 39, 1001]
+
--- 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/06-confirm-the-ending.js
+++ 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/06-confirm-the-ending.js
@@ -1,15 +1,16 @@
-/*
-  Challenge: Check if a string (first argument, str) ends with the given target string (second argument, target).
-  This challenge can be solved with the .endsWith() method, which was introduced in ES2015.
-  But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
-*/
+/**
+ * Challenge: Check if a string (first argument, str) ends with the given target string (second argument, target).
+ *
+ * This challenge can be solved with the .endsWith() method, which was introduced in ES2015.
+ * But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+ */
 
 function confirmEnding(str, target) {
   // return str.endsWith(target); // Using the built-in method
-  return str.slice(str.length - target.length) === target; // Using slice
+  return str.slice(str.length - target.length) === target;
 
   // Solution 2: Using substr, can also remove the str.length
   // return str.substr(str.length - target.length) === target;
 }
 
-confirmEnding("Bastian", "n"); // true
+confirmEnding('Bastian', 'n'); // true
