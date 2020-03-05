fdfdbe49857ef697ea2017583f7e33b1df57d973
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-03-05 23:59:58 +0800

    Update algorithm section
    - Repeat a String Repeat a String
    - Truncate a String

--- 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/07-repeat-a-string-repeat-a-string.js
+++ 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/07-repeat-a-string-repeat-a-string.js
@@ -1,26 +1,42 @@
-/*
-  Challenge: Repeat a given string str (first argument) for num times (second argument).
-  Return an empty string if num is not a positive number.
-*/
+/**
+ * Challenge: Repeat a given string str (first argument) for num times (second argument).
+ * Return an empty string if num is not a positive number.
+ */
 
+
+// Solution 1: Using while loop
 function repeatStringNumTimes(str, num) {
-  // return (num > 0) ? str.repeat(num) : ''; // Using built-in repeat & ternary
-  let result = '';
+  // Step 1: Create an empty str to store the repeated str
+  let repeatedStr = '';
 
-  for (let i = 1; i <= num; i++) {
-    result += str;
+  // Step 2: Set while as long as num is greater than 0
+  while (num > 0) {
+    repeatedStr += str;
+    num--; // decrement num
   }
-
-  // Solution 2: Using while
-  // while (num > 0) {
-  //   result += str;
-  //   num--;
-  // }
-
-  return result;
-
-  // Solution 3: Using conditional, recursion & ternary
-  // return (num < 0) ? '' : (num === 1) ? str : str + repeatStringNumTimes(str, num - 1);
+  // Step 3: Return the repeatedStr
+  return repeatedStr;
 }
 
-repeatStringNumTimes("abc", 3); // "abcabcabc"
+// Solution 2: Using for loop
+function repeatStringNumTimes(str, num) {
+  // Step 1: Create an empty str to store the repeated str
+  let repeatedStr = '';
+
+  // Step 2: Use for loop as long as i less than or equal to num, then add str
+  for (let i = 1; i <= num; i++) {
+    repeatedStr += str;
+  }
+  // Step 3: Return the repeatedStr
+  return repeatedStr;
+}
+
+// Solution 3: Using Recursion
+function repeatStringNumTimes(str, num) {
+  // Step 1: Check if num is greater than 0
+  // Step 2: If true, use recursion
+  // Step 3: Else (means num <= 0), then return empty string
+  return num > 0 ? str + repeatStringNumTimes(str, num - 1) : '';
+}
+
+repeatStringNumTimes('abc', 3); // abcabcabc
--- 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/08-truncate-a-string.js
+++ 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/08-truncate-a-string.js
@@ -1,12 +1,23 @@
-/*
-  Challenge: Truncate a string (first argument) if it is longer than the given maximum string length (second argument).
-  Return the truncated string with a ... ending.
-*/
+/**
+ * Challenge: Truncate a string (first argument) if it is longer than the given maximum string length (second argument).
+ * Return the truncated string with a ... ending.
+ */
+
+// Using slice() method
+function truncateString(str, num) {
+  // Step 1: Check if str.length is less than or equal to num, if true return str (don't truncate)
+  if (str.length <= num) {
+    return str
+  }
+
+  // Step 2: Return str truncated with '...' concatenated to the end of str
+  return str.slice(0, num) + '...'
+}
+
+// Using slice method and ternary
 
 function truncateString(str, num) {
   return str.length > num ? str.slice(0, num) + '...' : str; // Using slice & ternary
 
   // return str.length > num ? str.substr(0, num) + '...' : str; // Using substr or substring & ternary
 }
-
-truncateString("A-tisket a-tasket A green and yellow basket", 8); // "A-tisket..."
