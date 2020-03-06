d02b82c4498e4424475f689cf637c662522b4270
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-03-06 23:04:22 +0800

    Fix finders keepers and boo who

--- 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/09-finders-keepers.js
+++ 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/09-finders-keepers.js
@@ -1,18 +1,34 @@
-/*
-  Challenge: Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
-  If no element passes the test, return undefined.
-*/
+/**
+ * Challenge: Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+ * If no element passes the test, return undefined.
+ */
 
+// Solution 1: Using for loop
 function findElement(arr, func) {
   let num;
-  
+
   for(let i = 0; i < arr.length; i++) {
     num = arr[i];
     if (func(num)) return num;
-
-    // Solution 2
-    // if (func(arr[i])) return num; // or func(arr[i]) === true)
   }
 }
 
-console.log(findElement([1, 2, 3, 4], num => num % 4 === 0)); // 4
+// Solution 2: Using find() method
+function findElement(arr, func) {
+  // find() will stop the loop once the value is found
+  return arr.find(func);
+}
+
+// Solution 3: Using filter() method
+function findElement(arr, func) {
+  // Gets the first elem from arr returned by filter() (targetting index[0])
+  let [a] = arr.filter(num => func(num));
+  return a;
+}
+
+// Solution 4: Using map() and indexOf() method
+function findElement(arr, func) {
+  return arr[arr.map(func).indexOf(true)];
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0); // 2
--- 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/10-boo-who.js
+++ 02-javascript-algorithms-and-data-structures/06-basic-algorithm-scripting/10-boo-who.js
@@ -1,14 +1,20 @@
-/*
-  Challenge: Check if a value is classified as a boolean primitive. Return true or false.
-  Boolean primitives are true and false.
-*/
+/**
+ * Challenge: Check if a value is classified as a boolean primitive.
+ * Return true or false.
+ *
+ * Boolean primitives are true and false.
+ */
 
+// Solution 1: Using ternary operator
 function booWho(bool) {
-  // Adding ternary operator is optional because it will always evaluate the first to true then false
-  return (bool === true || bool === false); // ? true : false optional
-  
-  // return typeof bool === 'boolean'; // Using typeof
+  // Adding ternary operator is optional because it will always evaluate the condition first to true then false
+  return bool === true || bool === false; // ? true : false are optional
+}
 
+// Solution 2: Using typeof operator
+function booWho(bool) {
+  // Check the typeof bool if it's equal true, return true, else return false
+  return typeof bool === 'boolean';
 }
 
 booWho(null); // false
