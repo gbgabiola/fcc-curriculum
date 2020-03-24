66ca6064459d4fd9d63bcfe41c49abd339254239
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-03-25 05:18:17 +0800

    Update technical documentation page

--- 02-javascript-algorithms-and-data-structures/08-functional-programming/06-refactor-global-variables-out-of-functions.js
+++ 02-javascript-algorithms-and-data-structures/08-functional-programming/06-refactor-global-variables-out-of-functions.js
@@ -1,30 +1,24 @@
-/*
-  Challenge: Refactor (rewrite) the code so the global array bookListis not changed inside either function.
-  The addfunction should add the given bookNameto the end of an array.
-  The removefunction should remove the given bookNamefrom an array.
-  Both functions should return an array, and any new parameters should be added before the bookNameone.
-*/
+/**
+ * Challenge: Rewrite the code so the global array bookList is not changed inside either function.
+ * The add function should add the given bookName to the end of the array passed to it and return a new array (list).
+ * The remove function should remove the given bookName from the array passed to it.
+ *
+ * Note: Both functions should return an array, and any new parameters should be added before the bookName parameter.
+ */
 
-// the global variable
-var bookList = [
-  'The Hound of the Baskervilles',
-  'On The Electrodynamics of Moving Bodies',
-  'Philosophiæ Naturalis Principia Mathematica',
-  'Disquisitiones Arithmeticae'
-];
+// The global variable
+var bookList = ['The Hound of the Baskervilles', 'On The Electrodynamics of Moving Bodies', 'Philosophiæ Naturalis Principia Mathematica', 'Disquisitiones Arithmeticae'];
 
-/* This function adds a book to the list and return the list */
 function add(arr, bookName) {
-  // let newArr = [...arr];
+  // const newArr = [...arr];
   // newArr.push(bookName);
   // return newArr;
 
   return [...arr, bookName];
 }
 
-/* This function removes a book from the list and return the list */
 function remove(arr, bookName) {
-  // let newArr = [...arr];
+  // const newArr = [...arr];
   // if (newArr.indexOf(bookName) >= 0) {
   //   newArr.splice(newArr.indexOf(bookName), 1);
   //   return newArr;
@@ -37,9 +31,6 @@ function remove(arr, bookName) {
 
 var newBookList = add(bookList, 'A Brief History of Time');
 var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
-var newestBookList = remove(
-  add(bookList, 'A Brief History of Time'),
-  'On The Electrodynamics of Moving Bodies'
-);
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
 
-console.log(bookList); // (4) ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"]
+console.log(bookList); // [ 'The Hound of the Baskervilles', 'On The Electrodynamics of Moving Bodies', 'Philosophiæ Naturalis Principia Mathematica', 'Disquisitiones Arithmeticae']
--- 02-javascript-algorithms-and-data-structures/08-functional-programming/08-implement-map-on-a-prototype.js
+++ 02-javascript-algorithms-and-data-structures/08-functional-programming/08-implement-map-on-a-prototype.js
@@ -1,7 +1,7 @@
-/*
-  Challenge: Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map().
-  You may use a forloop or the forEachmethod.
-*/
+/**
+ * Challenge: Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map().
+ * You may use a for loop or the forEach method.
+ */
 
 // the global Array
 var s = [23, 65, 98, 5];
@@ -18,7 +18,4 @@ var new_s = s.myMap(function(item) {
   return item * 2;
 });
 
-// 46
-// 46,130
-// 46,130,196
-// 46,130,196,10
+// [46, 130, 196, 10]
