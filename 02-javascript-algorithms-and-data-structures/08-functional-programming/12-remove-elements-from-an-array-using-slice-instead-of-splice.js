753b26bb36fa43e23a91e42cca9b865ee0ff0ce5
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-03-27 23:22:39 +0800

    Update functional section
    - Return Part of an Array Using the slice Method
    - Remove Elements from an Array Using slice Instead of splice
    - Combine Two Arrays Using the concat Method

--- 02-javascript-algorithms-and-data-structures/08-functional-programming/12-remove-elements-from-an-array-using-slice-instead-of-splice.js
+++ 02-javascript-algorithms-and-data-structures/08-functional-programming/12-remove-elements-from-an-array-using-slice-instead-of-splice.js
@@ -1,12 +1,13 @@
-/*
-  Challenge: Rewrite the function nonMutatingSplice by using slice instead of splice.
-  It should limit the provided cities array to a length of 3, and return a new array with only the first three items.
-
-  Do not mutate the original array provided to the function.
-*/
+/**
+ * Challenge: Rewrite the function nonMutatingSplice by using slice instead of splice.
+ * It should limit the provided cities array to a length of 3, and return a new array with only the first three items.
+ *
+ * Do not mutate the original array provided to the function.
+ */
 
 function nonMutatingSplice(cities) {
   return cities.slice(0, 3);
 }
-var inputCities = ['Chicago', 'Delhi', 'Islamabad', 'London', 'Berlin'];
+
+const inputCities = ['Chicago', 'Delhi', 'Islamabad', 'London', 'Berlin'];
 nonMutatingSplice(inputCities); // ['Chicago', 'Delhi', 'Islamabad']
--- 02-javascript-algorithms-and-data-structures/08-functional-programming/13-combine-two-arrays-using-the-concat-method.js
+++ 02-javascript-algorithms-and-data-structures/08-functional-programming/13-combine-two-arrays-using-the-concat-method.js
@@ -1,11 +1,11 @@
-/*
-  Challenge: Use the concat method in the nonMutatingConcat function to concatenate attach to the end of original.
-  The function should return the concatenated array.
-*/
+/**
+ * Challenge: Use the concat method in the nonMutatingConcat function to concatenate attach to the end of original.
+ * The function should return the concatenated array.
+ */
 
 function nonMutatingConcat(original, attach) {
   return original.concat(attach);
 }
-var first = [1, 2, 3];
-var second = [4, 5];
+const first = [1, 2, 3];
+const second = [4, 5];
 nonMutatingConcat(first, second); // [1, 2, 3, 4, 5]
